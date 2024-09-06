27b7471116c64cd95d3d49422def666f25aae31b
TontonBanton <pancla13ulas@gmail.com>
2024-09-06 10:39:08 +0800

    Save Job on Mock JSON  API

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { reactive } from 'vue'
+import axios from 'axios'
+import { useRouter } from 'vue-router';
+const router = useRouter()
 
 const form = reactive({
-  type: 'Part-Time',
+  type: '',
   title: '',
   description: '',
-  salary: 'Under $50K',
+  salary: '',
   location: '',
   company: {
     name: '',
@@ -16,8 +19,29 @@ const form = reactive({
 })
 
 const handleSubmit = async ()=> {
-  alert(form.title)
+
+  const newJob = {
+    type: form.type,
+    title: form.title,
+    description: form.description,
+    salary: form.salary,
+    location: form.location,
+    company: {
+      name: form.company.name,
+      description: form.company.description,
+      contactEmail: form.company.contactEmail,
+      contactPhone: form.company.contactPhone
+    }
+  }
+  console.log(newJob)
+  try {
+    const response = await axios.post('/api/jobs', newJob)
+    router.push(`/jobs/${response.data.id}`)
+  } catch (error) {
+    console.error('Error fetching: ', error)
+  }
 }
+
 </script>
 
 <template>
@@ -28,7 +52,8 @@ const handleSubmit = async ()=> {
             <h2 class="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
             <div class="mb-4"><label for="type" class="block text-gray-700 font-bold mb-2">Job Type</label>
-              <select v-model="form.type" id="type" name="type" class="border rounded w-full py-2 px-3" required>
+              <select id="type" name="type" class="border rounded w-full py-2 px-3" required
+                v-model="form.type">
                 <option value="Full-Time">Full-Time</option>
                 <option value="Part-Time">Part-Time</option>
                 <option value="Remote">Remote</option>
@@ -38,17 +63,20 @@ const handleSubmit = async ()=> {
 
             <div class="mb-4">
               <label class="block text-gray-700 font-bold mb-2">Job Listing Name</label>
-              <input v-model="form.title" type="text" id="name" name="name" class="border rounded w-full py-2 px-3 mb-2" placeholder="eg. Listed Job" required />
+              <input type="text" id="name" name="name" class="border rounded w-full py-2 px-3 mb-2" placeholder="eg. Listed Job" required
+                v-model="form.title" />
             </div>
 
             <div class="mb-4">
               <label for="description" class="block text-gray-700 font-bold mb-2">Description</label>
-              <textarea v-model="form.description" id="description" name="description" class="border rounded w-full py-2 px-3" rows="4" placeholder="Duties, Requirements, etc"></textarea>
+              <textarea id="description" name="description" class="border rounded w-full py-2 px-3" rows="4" placeholder="Duties, Requirements, etc"
+                v-model="form.description"></textarea>
             </div>
 
             <div class="mb-4">
               <label for="type" class="block text-gray-700 font-bold mb-2">Salary</label>
-              <select v-model="form.salary" id="salary" name="salary" class="border rounded w-full py-2 px-3" required>
+              <select id="salary" name="salary" class="border rounded w-full py-2 px-3" required
+                v-model="form.salary">
                 <option value="Under $50K">under $50K</option>
                 <option value="$50K - $60K">$50 - $60K</option>
                 <option value="$60K - $70K">$60 - $70K</option>
@@ -65,27 +93,32 @@ const handleSubmit = async ()=> {
 
             <div class="mb-4">
               <label class="block text-gray-700 font-bold mb-2">Location</label>
-              <input v-model="form.location" type="text" id="location" name="location" class="border rounded w-full py-2 px-3 mb-2" placeholder="Adresss" required />
+              <input type="text" id="location" name="location" class="border rounded w-full py-2 px-3 mb-2" placeholder="Adresss" required
+                v-model="form.location"/>
             </div>
 
             <h3 class="text-2xl mb-5">Company Info</h3>
             <div class="mb-4">
               <label for="company" class="block text-gray-700 font-bold mb-2">Company Name</label>
-              <input v-model="form.company.name" type="text" id="company" name="company" class="border rounded w-full py-2 px-3" placeholder="Company Name"/>
+              <input type="text" id="company" name="company" class="border rounded w-full py-2 px-3" placeholder="Company Name"
+                v-model="form.company.name"/>
             </div>
 
             <div class="mb-4">
               <label for="company_description" class="block text-gray-700 font-bold mb-2">Company Description</label>
-              <textarea v-model="form.company.description" id="company_description" name="company_description" class="border rounded w-full py-2 px-3" rows="4" placeholder="What company do?"></textarea>
+              <textarea id="company_description" name="company_description" class="border rounded w-full py-2 px-3" rows="4" placeholder="What company do?"
+                v-model="form.company.description"></textarea>
             </div>
 
             <div class="mb-4">
               <label for="contact_email" class="block text-gray-700 font-bold mb-2">Contact Email</label>
-              <input v-model="form.company.contactEmail" type="email" id="contact_email" name="contact_email" class="border rounded w-full py-2 px-3" placeholder="Email address" />
+              <input type="email" id="contact_email" name="contact_email" class="border rounded w-full py-2 px-3" placeholder="Email address"
+                v-model="form.company.contactEmail"/>
             </div>
             <div class="mb-4">
               <label for="contact_phone" class="block text-gray-700 font-bold mb-2" >Contact Phone</label>
-              <input v-model="form.company.contactPhone" type="tel" id="contact_phone" name="contact_phone" class="border rounded w-full py-2 px-3" placeholder="Optional phone"/>
+              <input type="tel" id="contact_phone" name="contact_phone" class="border rounded w-full py-2 px-3" placeholder="Optional phone"
+                v-model="form.company.contactPhone"/>
             </div>
 
             <div>
